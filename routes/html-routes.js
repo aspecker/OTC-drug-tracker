5b64a15e951eafaa52946511ce7b54c08e9096ff
Adam Specker <34694802+aspecker@users.noreply.github.com>
2018-03-26 10:18:30 -0400

    updated hmtl routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,27 +1,26 @@
-const path = require("path");
-const isAuthenticated = require("../config/middleware/isAuthenticated");
+var path = require("path");
+
+var isAuthenticated = require("/config/middleware/isAuthenticated");
+
 module.exports = function(app) {
+
   app.get("/", function(req, res) {
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
-  app.get("/api/add", function(req, res) {
-    console.log(req);
-    console.log(res);
-    // if (req.user) {
-    //   res.redirect("/members");
-    // }
-    res.sendFile(path.join(__dirname, "../index.html"));
+
+  app.get('/search',function(req,res){
+    res.sendFile(path.join(__dirname, '/public/search.html'))
+  })
+
+  app.get("/login", function(req, res) {
+    if (req.user) {
+      res.redirect("/meds");
+    }
+    res.sendFile(path.join(__dirname, "/public/login.html"));
   });
-  // app.get("/login", function(req, res) {
-  //   if (req.user) {
-  //     res.redirect("/members");
-  //   }
-  //   res.sendFile(path.join(__dirname, "../public/login.html"));
-  // });
-  // app.get("/meds", isAuthenticated, function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/members.html"));
-  // });
+
+  app.get("/meds", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/members.html"));
+  });
+
 };
