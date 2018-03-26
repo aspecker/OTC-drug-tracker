508bccfdb0ea0e925766956388291b78a9c56f49
Xhandler <chandlerd8@gmail.com>
2018-03-26 10:14:20 -0400

    updated api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,28 +5,43 @@ const passport = require("../config/passport");
 
 module.exports = function(app) {
 
-	app.post("/api/search", (req, res) => {
+	app.get("/api/search", (req, res) => {
 
-		db.Med.findAll({}).then(results => {
-				res.json(results);
-		})
+		db.Med.findAll({}).then(results => res.json(results));
+
 	});
 	app.post("/api/add", (req, res) => {
-		// const drugInfo = req.body.results[0]
+		// const drugInfo = req.body.res[0]
 		db.Med.create({
-			userId: 2,
-							//db.User.id
-			fdaMedId: "ksnadkjnaskjdnaksj"
-							//drugInfo.openfda.product_ndc[0]
-		}).then(() => { 
-			//Refresh somehow
-		})
+			userId: 3267,
+			//db.User.id
+			fdaMedId: "ksnadkjnaskjdnaksj" + Math.floor(Math.random() * 1000),
+			// drugInfo.openfda.product_ndc[0]
+
+			brandName: "sdklsjakdj",
+
+			isTaking: true
+			//drugInfo.openfda.product
+		}).then(results => res.json(results))
 	});
-	app.post("/api/user/", (req, res) => {
+	app.put("/api/update", (req, res) => {
+		db.Med.update({
+
+			isTaking: false
+			// req,isTaking
+		}, {
+			where: {
+				id: 1
+				//req.id ?
+			}
+
+		}).then(() => null)
+
+	})
+	app.get("/api/user", (req, res) => {
 		db.Med.findAll({
 			where: {
-				userId: 1
-				 				//db.User.id - dtnamic value
+				brandName: "sdklsjakdj", //db.User.id - dtnamic value
 			}
 		}).then(userMeds => res.json(userMeds))
 	});
