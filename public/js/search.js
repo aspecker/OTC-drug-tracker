e00ba5c75c1d44619ed1d16e25b02de545e03e49
Xhandler <chandlerd@gmail.com>
2018-03-28 16:22:21 -0400

    better error handling on search

--- public/js/search.js
+++ public/js/search.js
@@ -1,8 +1,8 @@
 const upperCase = (value) => {
 	// Build up result.
-	var result = "";
+	let result = "";
 	// Loop over string indexes.
-	for (var i = 0; i < value.length; i++) {
+	for (let i = 0; i < value.length; i++) {
 		// Get char code at this index.
 		var code = value.charCodeAt(i);
 		// For first character, or a lowercase range char following a space.
@@ -23,6 +23,8 @@ const upperCase = (value) => {
 //
 const check = () => {
 	$('.add-btn').on('click', function() {
+		$(this).prop("disabled", true);
+		alert("added your mom maannnnnnn")
 		var medArr = this.value.split('^');
 		console.log(medArr);
 		$.post('/api/add', {
@@ -34,31 +36,40 @@ const check = () => {
 	});
 }
 $('#searchBtn').on('click', async function() {
-	$("#searchResults").empty();
-	let searchTerm = $('#fdaSearch').val();
-	console.log(searchTerm);
-	$.get(`https://api.fda.gov/drug/label.json?search=openfda.product_type:otc+AND+brand_name:${searchTerm}&limit=5`,
-		(req, res) => {
-			let obj = req.results;
-			// let parsedData = [];
-			Object.keys(obj).forEach(function(key) {
-				let brandName = upperCase(obj[key].openfda.brand_name[0].toLowerCase());
-				let genericName = upperCase(obj[key].openfda.generic_name[0].toLowerCase());
-				let route = obj[key].openfda.route[0];
-				let purpose = obj[key].purpose[0];
-				let doseAdmin = obj[key].dosage_and_administration[0];
-				let activeIngredient = obj[key].active_ingredient[0];
-				let fdaMedId = obj[key].openfda.product_ndc[0];
-				let pullData = [`${fdaMedId}^${brandName}^${genericName}^${activeIngredient}`];
-				console.log(pullData);
-				$("#searchResults").append(
-					`<tr><td>${brandName}</td><td>${genericName}</td><td>${purpose}</td><td>${doseAdmin}</td><td>${activeIngredient}</td><td><button class="add-btn btn btn-danger btn-block" id="${key}" value='${pullData}'>Click</button></td></tr><`
-				);
+			$("#searchResults").empty();
+			$("#error").empty();
+			let searchTerm = $('#fdaSearch').val();
+			// console.log(searchTerm);
+			$.ajax({
+					url: `https://api.fda.gov/drug/label.json?search=openfda.product_type:otc+AND+brand_name:${searchTerm}&limit=5`,
+					type: 'GET',
+					error: function(xhr, ajaxOptions, thrownError) {
+						if (xhr.status == 404) {
+							$("#error").html("Drug not found, please check spelling");
+						}
+					}
+					}).then((req, res) => {
 
-			});
-			check();
-		})
+					let obj = req.results;
+					// let parsedData = [];
+					Object.keys(obj).forEach(function(key) {
+						let brandName = upperCase(obj[key].openfda.brand_name[0].toLowerCase());
+						let genericName = upperCase(obj[key].openfda.generic_name[0].toLowerCase());
+						let route = obj[key].openfda.route[0];
+						let indications = obj[key].indications_and_usage[0];
+						// let doseAdmin = obj[key].dosage_and_administration[0];
+						let activeIngredient = obj[key].active_ingredient[0];
+						let fdaMedId = obj[key].openfda.product_ndc[0];
+						let pullData = [`${fdaMedId}^${brandName}^${genericName}^${activeIngredient}`];
+						// console.log(pullData);
+						$("#searchResults").append(
+							`<tr><td>${brandName}</td><td>${genericName}</td><td>${indications}</td><td>${activeIngredient}</td><td><button class="add-btn btn btn-danger btn-block" id="${key}" value='${pullData}'>Click</button></td></tr><`
+						);
+					});
+					check();
+				})
 
+<<<<<<< Updated upstream
 })
 
 $('#fdaSearch').keypress(function(e){
@@ -66,3 +77,6 @@ $('#fdaSearch').keypress(function(e){
 		$('#searchBtn').click();//Trigger search button click event
 	}
 });
+=======
+			})
+>>>>>>> Stashed changes
