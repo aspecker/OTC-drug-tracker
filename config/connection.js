3de1c39c188bd6898025aaec8524eb110baf7003
Adam Specker <specker@gmail.com>
2018-03-29 11:44:14 -0400

    heroku deploy

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'otc_tracker'
+    });
+};
+
+connection.connect();
+module.exports = connection;
