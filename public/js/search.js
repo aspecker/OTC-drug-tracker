dc2e09ed0fb3d74183e446e0fa609985f439605b
Adam Specker <specker@gmail.com>
2018-03-29 10:25:45 -0400

    bug fix

--- public/js/search.js
+++ public/js/search.js
@@ -49,41 +49,6 @@ $('#searchBtn').on('click', async function() {
 	$("#error").empty();
 	let searchTerm = $('#fdaSearch').val();
 
-	console.log(searchTerm);
-	$.get(`https://api.fda.gov/drug/label.json?search=openfda.product_type:otc+AND+brand_name:${searchTerm}&limit=5`,
-		(req, res) => {
-			let obj = req.results;
-			// let parsedData = [];
-			Object.keys(obj).forEach(function(key) {
-				let brandName = obj[key].openfda.brand_name[0];
-				let genericName = obj[key].openfda.generic_name[0];
-				let route = obj[key].openfda.route[0];
-				// let whenUsing = obj[key].when_using[0];
-				let purpose = obj[key].purpose[0];
-				let doseAdmin = obj[key].dosage_and_administration[0];
-				let activeIngredient = obj[key].active_ingredient[0];
-				// let question = obj[key].questions[0];
-				let fdaMedId = obj[key].openfda.product_ndc[0];
-				let pullData = [`${fdaMedId}^${brandName}^${genericName}^${activeIngredient}`];
-				console.log(pullData);
-				$("#searchResults").append(
-					`<tr><td>${brandName}</td><td>${genericName}</td><td>${purpose}</td><td>${doseAdmin}</td><td>${activeIngredient}
-					</td>
-					<button class="add-btn btn btn-info btn-block" id="${key}" value='${pullData}'>Add</button>
-					
-					</tr><hr>`
-				);
-				// $(`#${key}`).data('savedMed', {brand_name: brandName, generic_name: genericName, rout: route} );
-				// parsedhttps://api.fda.gov/drug/label.json?search=openfda.product_type:otc+AND+brand_name:${searchTerm}&limit=5`Data.push([brandName, genericName, route, whenUsing, purpose, doseAdmin, activeIngredient, question]);
-				// parsedData.push([brandName, genericName]);
-				// console.log(brandName)
-			});
-			check();
-			// console.log(parsedData);
-			// document.write(parsedData);
-		})
-
-	// console.log(searchTerm);
 	$.ajax({
 		url: `https://api.fda.gov/drug/label.json?search=openfda.product_type:otc+AND+brand_name:${searchTerm}&limit=5`,
 		type: 'GET',
