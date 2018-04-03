9325a9aadcedd33481c5e1befa17fa4d5afa787b
Xhandler <chandlerd@gmail.com>
2018-04-03 18:53:09 -0400

    Fixed navbar --needs to be updated

--- public/js/search.js
+++ public/js/search.js
@@ -1,18 +1,12 @@
+// handles variable casing for adding brand name to the dbcol-sm-6
 const upperCase = (value) => {
-	// Build up result.
 	let result = "";
-	// Loop over string indexes.
 	for (let i = 0; i < value.length; i++) {
-		// Get char code at this index.
 		var code = value.charCodeAt(i);
-		// For first character, or a lowercase range char following a space.
-		// ... The value 97 means lowercase A, 122 means lowercase Z.
 		if (i === 0 ||
 			value[i - 1] === " " &&
 			code >= 97 &&
 			code <= 122) {
-			// Convert from lowercase to uppercase by subtracting 32.
-			// ... This uses ASCII values.
 			result += String.fromCharCode(code - 32);
 		} else {
 			result += value[i];
@@ -20,7 +14,7 @@ const upperCase = (value) => {
 	}
 	return result;
 }
-//
+
 const check = () => {
 	$.get('/api/check', (data, status) => {
 		if (!data) {
@@ -29,9 +23,13 @@ const check = () => {
 			return;
 		} else {
 			$('.add-btn').on('click', function() {
-				$(this).prop("disabled", true);
-				alert("Medicine was added.")
 				var medArr = this.value.split('^');
+				$('.modal-title').empty();
+				$('.modal-body').empty();
+				$('.modal-title').html('<h2>Medicine added.</h2>');
+				$('.modal-body').html(`<h4>${medArr[1]} has been added to your records.</h4>`);
+				$('#warning').modal('show');
+				$(this).prop("disabled", true);
 				console.log(medArr);
 				$.post('/api/add', {
 					id: medArr[0],
@@ -42,36 +40,33 @@ const check = () => {
 			});
 		}
 	})
-
 }
+
+
 $('#searchBtn').on('click', async function() {
 	$("#searchResults").empty();
 	$("#error").empty();
 	let searchTerm = $('#fdaSearch').val();
-
 	$.ajax({
 		url: `https://api.fda.gov/drug/label.json?search=openfda.product_type:otc+AND+brand_name:${searchTerm}&limit=5`,
 		type: 'GET',
 		error: (xhr, ajaxOptions, thrownError) => {
 			if (xhr.status == 404) return $("#error").html("Drug not found, please check spelling");
-			if (xhr.status == 500) return $("#error").html("Whoops! Something we did went wrong, plase try again.")
+			if (xhr.status == 500) return $("#error").html("Whoops! Something's wrong on our end. Please try again.")
 		}
 	}).then((req, res) => {
-
+		$('.table').show();
 		let obj = req.results;
-		// let parsedData = [];
 		Object.keys(obj).forEach(function(key) {
 			let brandName = upperCase(obj[key].openfda.brand_name[0].toLowerCase());
 			let genericName = upperCase(obj[key].openfda.generic_name[0].toLowerCase());
 			let route = obj[key].openfda.route[0];
 			let indications = obj[key].indications_and_usage[0];
-			// let doseAdmin = obj[key].dosage_and_administration[0];
 			let activeIngredient = obj[key].active_ingredient[0];
 			let fdaMedId = obj[key].openfda.product_ndc[0];
 			let pullData = [`${fdaMedId}^${brandName}^${genericName}^${activeIngredient}`];
-			// console.log(pullData);
 			$("#searchResults").append(
-				`<tr><td>${brandName}</td><td>${genericName}</td><td>${indications}</td><td>${activeIngredient}</td><td><a class="loginAnchor"><button class="add-btn btn btn-danger btn-block" id="${key}" value='${pullData}'>Add</button></a></td></tr><`
+				`<tr><td><h4>${brandName}</h4></td><td>${genericName}</td><td>${indications}</td><td>${activeIngredient}</td><td><a class="loginAnchor"><button class="add-btn btn btn-danger btn-block" id="${key}" value='${pullData}'>Add</button></a></td></tr><`
 			);
 		});
 		check();
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,52 +1,76 @@
-
 var path = require("path");
 const db = require("../models");
 
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
-  const checkUser = (req) => {
-    console.log(req.user ? req.user.email : "Sign In");
-    return req.user ? req.user.email : "Sign In";
-  }
+	const checkUser = (req) => {
+		const isSignedOn = req.user ? true : false;
+		if (!req.user) {
+			let data = {
+				"active": false,
+				"message": "Sign In"
+			}
+			return data;
+		};
+		return {
+			"active": true,
+			"message": req.user.email
+		};
+	}
 
-  // renders index on root
-  app.get("/", function(req, res) {
-    res.render("index", {username: checkUser(req)});
-  });
+	// renders index on root
+	app.get("/", function(req, res) {
+		res.render("index", {
+      isActive: checkUser(req).active,
+			message: checkUser(req).message
+		});
+	});
 
-  //login route with redirect to meds
-  app.get("/login", function (req, res) {
-    if (req.user) {
-      return res.redirect("/meds");
-    }
-    else {
-      res.render("login");
-    }
-  });
+	//login route with redirect to meds
+	app.get("/login", function(req, res) {
+		if (req.user) {
+			return res.redirect("/meds");
+		} else {
+			res.render("login", {
+        isActive: checkUser(req).active,
+  			message: checkUser(req).message
+			});
+		}
+	});
 
-  // signup page route
-  app.get('/signup', (req,res)=>{
-    res.render('signup')
-  })
+	// signup page route
+	app.get('/signup', (req, res) => {
+		console.log(checkUser(req))
+		res.render('signup', {
+      isActive: checkUser(req).active,
+			message: checkUser(req).message
+		})
+	})
 
-  // med page route
-  app.get("/meds", isAuthenticated, function(req, res) {
-    // sequelize finds user meds
-    db.Med.findAll({where: {
-			userId: req.user.id
-		}}).then(results => {
-    // renders user meds using handlebars partial
-    res.render('mypillpal', {
-      username : checkUser(req),
-      meds: results
-    });
-    });
-  });
+	// med page route
+	app.get("/meds", isAuthenticated, function(req, res) {
+		// sequelize finds user meds
+		db.Med.findAll({
+			where: {
+				userId: req.user.id
+			}
+		}).then(results => {
+			// renders user meds using handlebars partial
+			res.render('mypillpal', {
+				isActive: checkUser(req).active,
+				message: checkUser(req).message,
+				meds: results
+			});
+		});
+	});
 
-  // search page route
-  app.get('/search', function(req,res){
-    res.render('search')
-  })
+	// search page route
+	app.get('/search', function(req, res) {
+		res.render('search', {
+      isActive: checkUser(req).active,
+			message: checkUser(req).message
+		})
+	})
 
 };
